9fe7689c8e6734db626dc8fd790cdb9397475ff2
cb-abd-dev <cb@abd.dev>
2021-11-28 20:47:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "test_app_1_1",
+  url: "https://test_app_1_1.botics.co",
+  api: "https://test_app_1_1.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
